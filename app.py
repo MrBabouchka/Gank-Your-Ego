2c92996a7af00e670e732d40edcb71b1c0923bad
Mr.Babouchka <cupcic.k@gmail.com>
2025-07-25 11:26:50 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Gank Your Ego", layout="centered")
+st.title("🧠 Gank Your Ego")
+st.write("Bienvenue dans ton coach LoL brutalement honnête.")
